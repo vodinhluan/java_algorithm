8f6166cb1484eb2d681b79066d8ce285574b45b4
vodinhluan <luandinhvo2002@gmail.com>
2024-07-01 15:37:30 +0700

    insert heap

--- Heap/Heap.java
+++ Heap/Heap.java
@@ -19,10 +19,23 @@ public class Heap {
         return index * 2 + 2;
     }
 
+    private int parent(int index) {
+        return (index-1) / 2;
+    }
+
     private void swap(int index1, int index2) {
         int temp = heap.get(index1);
         index1 = heap.set(index1, heap.get(index2));
         index2 = heap.set(index2, temp);
+    }
+
+    public void insert (int value) {
+        heap.add(value);
+        int current = heap.size() - 1;
+        while (current > 0 && heap.get(current) > heap.get(parent(current))) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
     }
     
 }
---
+++ Heap/MainHeap.java
@@ -0,0 +1,15 @@
+public class MainHeap {
+    public static void main(String[] args) {
+        Heap myHeap = new Heap();
+        myHeap.insert(99);
+        myHeap.insert(72);
+        myHeap.insert(61);
+        myHeap.insert(58);
+        System.out.println(myHeap.getHeap());
+
+        myHeap.insert(100);
+        System.out.println(myHeap.getHeap());
+        myHeap.insert(75);
+        System.out.println(myHeap.getHeap());
+    }
+}
