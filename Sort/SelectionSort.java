4a3735d38899aa90b90e1f141835f56827467357
vodinhluan <luandinhvo2002@gmail.com>
2024-07-05 19:30:02 +0700

    selection sort

---
+++ Sort/SelectionSort.java
@@ -0,0 +1,28 @@
+package Sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            if (i != minIndex) {
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {4, 2, 6, 5, 1, 3};
+        selectionSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
